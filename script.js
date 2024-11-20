c223a82107789e4efc8ae6625935211058ef56c7
oscar <oscar.burman005@gmail.com>
2024-11-20 12:46:49 +0100

    Find nearby stops

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Key: c1f80921-2d9d-484a-b297-a221cdd62746
+const apiKey = "c1f80921-2d9d-484a-b297-a221cdd62746";
 const output = document.getElementById("output");
 
 let latitude;
@@ -9,6 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 function init() {
   getLocation();
+  //   useFetchedStops();
 }
 
 function getLocation() {
@@ -23,6 +26,7 @@ function success(position) {
   latitude = position.coords.latitude;
   longitude = position.coords.longitude;
   output.textContent = `Latitude: ${latitude}, Longitude: ${longitude}`;
+  useFetchedStops();
 }
 
 function error(err) {
@@ -39,5 +43,42 @@ function error(err) {
     case err.UNKNOWN_ERROR:
       output.textContent = "An unknown error occurred.";
       break;
+  }
+}
+
+async function fetchStops(lat, long, maxNo = 100, radius = 100000, lang = "en") {
+  const baseUrl = "https://api.resrobot.se/v2.1/location.nearbystops";
+  const params = new URLSearchParams({
+    originCoordLat: lat,
+    originCoordLong: long,
+    accessId: apiKey,
+    maxNo: maxNo,
+    r: radius,
+    lang: lang,
+    format: "json",
+  });
+
+  const url = `${baseUrl}?${params.toString()}`;
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Network error! Status: ${response.status}`);
+    }
+    console.log("res: ", response);
+    const data = await response.json();
+    console.log("Data: ", data.stopLocationOrCoordLocation);
+    return data.stopLocationOrCoordLocation;
+  } catch (error) {
+    console.error("Error fetching nearby stops", error);
+    return [];
+  }
+}
+
+function useFetchedStops() {
+  if (latitude && longitude) {
+    fetchStops(latitude, longitude).then((stops) => {
+      console.log("Nearby stops: ", stops);
+    });
   }
 }
